2f83210dad4e6260f046201533dc28920d1d3b67
livanjr <livanjr@planprophet.com>
2023-11-15 20:20:48 -0500

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,39 @@ function App() {
   const [newTodo, setNewTodo] = useState("");
 
   const addTodo = (task) => {
+    if (!task) return;
     const newTodoList = [
       ...todos,
       { id: todos.length + 1, task: task, done: false },
     ];
 
-    setTodo(newTodoList)
+    setTodo(newTodoList);
   };
 
   const delteTodo = (id) => {
-    const newTodoList = todos.filter((item) => item.id!=id);
-    setTodo(newTodoList)
-  }
+    const newTodoList = todos.filter((item) => item.id != id);
+    setTodo(newTodoList);
+  };
+
+  const changeTodoState = (id, state) => {
+    const newTodoList = todos.map((item) => {
+      if (item.id === id) {
+        return { ...item, done: state };
+      } else {
+        return item;
+      }
+    });
+
+    setTodo(newTodoList);
+  };
 
   return (
     <div className="container">
       <h1>
-        {"<"}My To do list{"/>"}
+        🎯My To do list✏️
       </h1>
 
-      <div className="new-todo-container">
+      <form className="new-todo-container" onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           value={newTodo}
@@ -45,19 +58,23 @@ function App() {
         >
           ➕
         </button>
-      </div>
+      </form>
 
       <ul className="todo-list">
         {todos.map((item) => {
           return (
-            <li key={item.id} className="todo-item">
-              <input type="checkbox" value={item.done} />
+            <li key={item.id} className={`todo-item ${item.done?"done" : ""}`}>
+              <input
+                type="checkbox"
+                value={item.done}
+                onChange={(e) => {
+                  changeTodoState(item.id, e.target.checked);
+                }}
+              />
               <span className="todo-item-text">{item.task}</span>
-              <button 
-              onClick={()=> delteTodo(item.id)
-              }
-              className="delete"
-              >❌</button>
+              <button onClick={() => delteTodo(item.id)} className="delete">
+                ❌
+              </button>
             </li>
           );
         })}
